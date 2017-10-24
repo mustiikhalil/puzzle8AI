6d90219738ce296a8e51c407fc36f89c9ab75ebd
Mustafa Khalil <mustii@Mustafas-MacBook-Pro.local>
2017-10-24 07:39:49 +0300

    comments added

--- 8pzHw.py
+++ 8pzHw.py
@@ -1,8 +1,15 @@
 # Solves a randomized 8-puzzle using A* algorithm with plug-in heuristics
 
 """
-Name: Mustafa Khalil
+This is an implementation for the following algorithms Astar, DFS, and BFS
+It was implemented for the AI course in Antalya Bilim University.
+
+Implemented By Mustafa Khalil.
 ID: 140201100
+"""
+
+"""
+WARNING DFS will have a path retrival problem if the traversal tree is bigger than 1000 nodes
 
 """
 
@@ -15,41 +22,59 @@ finishState = [[0, 1, 2],
                [3, 4, 5],
                [6, 7, 8]]
 
-# finishState = [[1, 2, 3],
-#                [4, 5, 6],
-#                [7, 8, 0]]
 
-
-
+"""
+ A simple priority Queue implementation,
+ this implementation has a queue and a set.
+ to achive a check of O(1) to the queue while keeping the data sorted.
+"""
 class priorityQueue():
 
+    # constructor
     def __init__(self):
         self.queue = []
         self.set = {}
 
+    # add function
     def add(self,node,heur):
 
+        # if node is not in the set it adds it by hasing the value to the set and appending to the queue
         if node.__str__() not in self.set:
             self.set[node.__str__()] = True
-            self.queue.append((node,heur))
-            self.queue = sorted(self.queue, key=lambda x: x[1])
+            self.queue.append((node, heur))
+
+            # only if the heur is lower than the last item it would sort
+            if heur < self.queue[self.queue.__len__()-2][1]:
+                self.queue = sorted(self.queue, key=lambda x: x[1])
+
+        # if the node is already in the queue it updates it
         if node.__str__() in self.set:
             self.update(node,heur)
 
+
+    # update function for the queue
     def update(self, node, heur):
+
+        # if the node is already in the queue it updates it
         if node.__str__() in self.set:
             indx = [x for x, y in enumerate(self.queue) if y[0] == node]
             self.queue[indx[0]] = (node,heur)
             self.queue = sorted(self.queue, key=lambda x: x[1])
 
+
+    # pop function gets the element in the zero index
     def pop(self):
         popped = self.queue.pop(0)
+
+        # it removes it from the set
         del self.set[popped[0].__str__()]
         return popped
 
+    # gets the length of the queue
     def __len__(self):
         return self.queue.__len__()
 
+    # gets the second value in the tuple that is (node, heur)
     def getH(self,node):
         indx = [x for x, y in enumerate(self.queue) if y[0] == node]
         return self.queue[indx[0]][1]
@@ -72,7 +97,7 @@ class Puzzle8:
         # parent node in search path
         self._parent = None
         # the action (i.e., the direction of the move) that has applied to the parent node.
-        self.direction = None 
+        self.direction = None
         self.matrix = []
         for i in range(3):
             self.matrix.append(finishState[i][:])
@@ -89,12 +114,12 @@ class Puzzle8:
             res += ' '.join(map(str, self.matrix[row]))
             res += '\r\n'
         return res
-    
+
     def change_state(self, l):
         """ change the state of the current puzzle to l"""
         if len(l) != 9:
             print('length of the list should be 9')
-        self.matrix = []    
+        self.matrix = []
         for i in range(3):
             self.matrix.append(l[i*3:i*3+3])
 
@@ -107,7 +132,7 @@ class Puzzle8:
         return p
 
     def getLegalMoves(self):
-        """ Returns the set of available moves as a list of tuples, each tuple contains the row and col position 
+        """ Returns the set of available moves as a list of tuples, each tuple contains the row and col position
         with which the free space (0) may be swapped """
         # get row and column of the empty piece
         row, col = self.find(0)
@@ -125,8 +150,8 @@ class Puzzle8:
             free.append((row, col + 1, 'right' ))
 
         return free
-       
-    
+
+
     def generateMoves(self):
         """ Returns a set of puzzle objects that are successors of the current state   """
         free = self.getLegalMoves()
@@ -151,8 +176,6 @@ class Puzzle8:
 
             path.append(self)
             return self._parent.generateSolutionPath(path)
-
-
 
     def shuffle(self, step_count):
         """shuffles the puzzle by executing step_count number of random moves"""
@@ -187,11 +210,10 @@ class Puzzle8:
         temp = self.peek(*pos_a)
         self.poke(pos_a[0], pos_a[1], self.peek(*pos_b))
         self.poke(pos_b[0], pos_b[1], temp)
-        
+
     def isGoal(puzzle):
         """check if we reached  the goal state"""
         return puzzle.matrix == finishState
-
 
 
     ## Breath first Search algorithm to that will solve the puzzle by
@@ -234,7 +256,7 @@ class Puzzle8:
                 path.reverse()
 
                 ## exporting the information gathered for the question into a txt file.
-                file = open('output.txt', 'w')
+                file = open('BFSoutput.txt', 'w')
                 file.writelines(f'path_to_goal: {path}\n')
                 file.writelines(f'cost_of_path: {path.__len__()}\n')
                 file.writelines(f'nodes_expanded: {counter}\n')
@@ -311,7 +333,7 @@ class Puzzle8:
 
                 ## exporting the information gathered for the question into a txt file.
 
-                file = open('output.txt', 'w')
+                file = open('DFSoutput.txt', 'w')
                 file.writelines(f'path_to_goal: {path}\n')
                 file.writelines(f'cost_of_path: {path.__len__()}\n')
                 file.writelines(f'nodes_expanded: {counter}\n')
@@ -356,7 +378,14 @@ class Puzzle8:
 
         """Performs A* search for goal state. h(move) - heuristic function, returns an integer """
 
-
+    ## a Heuristic function that gets the amount of missplaced tiles in the game,
+    """
+    0 3 1
+    6 8 2
+    7 5 4
+    
+    returns 8
+    """
     def missPlacedHeuristic(self,node):
         count = 0
         if node.isGoal():
@@ -367,32 +396,56 @@ class Puzzle8:
                     count += 1
         return count
 
+    """
+        Astar search algorithm to be running on a very low O(n) in terms of time and space 
+    """
 
     def Astarsearch(self, h):
+
+        # a priorityQueue that was created in the top of the file
         frontier = priorityQueue()
+
+        # a set that holds all the explored Nodes
         exploredSet = {}
+
+        # Holds the depth of the explored node
         depth = 0
+
+        # counts the nodes explored
         counter = 0
+
+        # Checks if the self state is goal
         if self.isGoal():
-            print("didn't start")
             return True
 
+        # adds the first node with it's Heuristic value
         frontier.add(self, h(self))
 
         while frontier.__len__() != 0:
 
-
+            # popped tuple from the frontier
             popped = frontier.pop()
+
+            # appends the popped tuple node to node
             node = popped[0]
+
+            #sets the depth to the value explored
             depth = node._depth
+
+            # adds the node to explored
             exploredSet[node.__str__()] = True
             counter = counter + 1
+
+            # checks if the node is goal
             if node.isGoal():
+
                 path = []
                 for i in node.generateSolutionPath([]):
                     path.append(i.direction)
                 path.reverse()
-                file = open('output.txt', 'w')
+
+                # creates a file and outputs the moves, depth, length
+                file = open('Astaroutput.txt', 'w')
                 file.writelines(f'path_to_goal: {path}\n')
                 file.writelines(f'cost_of_path: {path.__len__()}\n')
                 file.writelines(f'nodes_expanded: {counter-1}\n')
@@ -401,18 +454,31 @@ class Puzzle8:
                 file.close()
 
                 return True
+
+            # generates the moves available
             for child in node.generateMoves():
+
+                # sets every childs Heuristic to itself
                 child._hval = h(child)
 
+                # checks if the child is not in explored or not in the frontier sets in O(1) time
                 if child.__str__() not in exploredSet and child.__str__() not in frontier.set:
+
+                    #adds to frontier (Node, Heuristic value)
                     frontier.add(child,child._hval +child._depth)
+
+                # checks if it's already in the frontier
                 elif child.__str__() in frontier.set:
+                    # checks if the current Heuristic value is less than the previous Heuristic value
                         if frontier.getH(child) > child._hval:
+
+                            # if true, update the node with the current Heuristic
                             frontier.update(child,child._hval+child._depth)
 
+        # returns false if the algorithm failed
         return False
 
-    """Performs A* search for goal state.        
+    """Performs A* search for goal state.
         h(move) - heuristic function, returns an integer
      """
 
@@ -428,7 +494,6 @@ def main():
     6 8 2
     7 5 4
     """
-
     p.change_state([0,3,1,6,8,2,7,5,4])
     print(p)
 
@@ -436,8 +501,10 @@ def main():
     print("-------")
     # print(p.DFS())
     print("-------")
-    # print (p.Astarsearch(p.missPlacedHeuristic))
+    print (p.Astarsearch(p.missPlacedHeuristic))
     # print(frontier)
+
+
 
 
 
